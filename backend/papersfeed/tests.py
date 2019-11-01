bb599a8721b8093d34195e062f72ad267c06e4c0
Davin <bdv951101@gmail.com>
2019-11-01 09:05:49 +0900

    minor changes for pylint

--- backend/papersfeed/tests.py
+++ backend/papersfeed/tests.py
@@ -1,7 +1,10 @@
+"""test.py"""
 from django.test import TestCase, Client
 
 class ApiEntryTestCase(TestCase):
-    def test_api_entry(self):
+    """api_entry test"""
+    def test_notfound(self):
+        """api_entry_test"""
         client = Client()
         response = client.get('/api/wrong')
         self.assertEqual(response.status_code, 404)
