b5c61cf016a3f5f1b1dc9987cf7bfe5cdd2b870c
Davin <bdv951101@gmail.com>
2019-11-01 09:26:18 +0900

    modify test comments

--- backend/papersfeed/tests.py
+++ backend/papersfeed/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase, Client
 class ApiEntryTestCase(TestCase):
     """api_entry test"""
     def test_notfound(self):
-        """api_entry_test"""
+        """response status should be 404 if wrong url"""
         client = Client()
         response = client.get('/api/wrong')
         self.assertEqual(response.status_code, 404)
