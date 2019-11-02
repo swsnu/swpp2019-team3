7cdbee16bd24bb9414c79116a1f5576fed1f29eb
Davin <bdv951101@gmail.com>
2019-11-02 11:16:24 +0900

    welcome message css

--- frontend/src/containers/Intro/Intro.js
+++ frontend/src/containers/Intro/Intro.js
@@ -9,7 +9,11 @@ class Intro extends Component {
     render() {
         return (
             <div className="intro-page">
-                <h1 id="welcome">Welcome to PAPERSFEED!</h1>
+                <div className="welcome-sentence">
+                    <h1 id="welcome-to">Welcome to&nbsp;</h1>
+                    <h1 id="papersfeed">PAPERSFEED</h1>
+                    <h1 id="exclamation-mark">!</h1>
+                </div>
                 <div className="buttons">
                     <Signup className="signup" history={this.props.history} />
                     <Signin className="signin" history={this.props.history} />
