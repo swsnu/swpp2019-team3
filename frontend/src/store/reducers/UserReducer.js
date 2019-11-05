f7e8ac013e38fa77dde9153b47223d30a9052fbe
kosumin <be_0302@naver.com>
2019-11-06 05:32:21 +0900

    to merge

--- frontend/src/store/reducers/UserReducer.js
+++ frontend/src/store/reducers/UserReducer.js
@@ -1,7 +1,7 @@
-import * as actionTypes from '../actions/ActionTypes';
+import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-    selectedUser : {
+    selectedUser: {
         id: -1,
         name: "",
         description: "",
@@ -12,39 +12,39 @@ const initialState = {
     },
     selectedFollowers: [],
     selectedFollowings: [],
-}
+};
 
 const UserReducer = (state = initialState, action) => {
-    switch (action.type){
-        case actionTypes.GET_USER:
-            const newSelectedUser = {
-                id: action.selectedUser.id,
-                name: action.selectedUser.name,
-                description: action.selectedUser.description,
-                followersNum: action.selectedUser.followersNum,
-                followingsNum: action.selectedUser.followingsNum,
-            };
-            return {...state, selectedUser: newSelectedUser };
-        case actionTypes.GET_FOLLOWERS:
-            return {...state, selectedFollowers: action.followers };
-        case actionTypes.GET_FOLLOWINGS:
-            return {...state, selectedFollowings: action.followings };
-        case actionTypes.ADD_FOLLOWING:
-            return {...state}; // not yet done
-        case actionTypes.DEL_FOLLOWING:
-            return {...state}; // not yet done
-        case actionTypes.EDIT_USER_PROFILE:
-            const editedUser = {
-                id: action.userEdited.id,
-                name: action.userEdited.name,
-                description: action.userEdited.description,
-                followersNum: action.userEdited.followersNum,
-                followingsNum: action.userEdited.followingsNum,
-            };
-            return {...state, selectedUser: editedUser };
-        default:
-            break;
-    };
+    switch (action.type) {
+    case actionTypes.GET_USER:
+        const newSelectedUser = {
+            id: action.selectedUser.id,
+            name: action.selectedUser.name,
+            description: action.selectedUser.description,
+            followersNum: action.selectedUser.followersNum,
+            followingsNum: action.selectedUser.followingsNum,
+        };
+        return { ...state, selectedUser: newSelectedUser };
+    case actionTypes.GET_FOLLOWERS:
+        return { ...state, selectedFollowers: action.followers };
+    case actionTypes.GET_FOLLOWINGS:
+        return { ...state, selectedFollowings: action.followings };
+    case actionTypes.ADD_FOLLOWING:
+        return { ...state }; // not yet done
+    case actionTypes.DEL_FOLLOWING:
+        return { ...state }; // not yet done
+    case actionTypes.EDIT_USER_PROFILE:
+        const editedUser = {
+            id: action.userEdited.id,
+            name: action.userEdited.name,
+            description: action.userEdited.description,
+            followersNum: action.userEdited.followersNum,
+            followingsNum: action.userEdited.followingsNum,
+        };
+        return { ...state, selectedUser: editedUser };
+    default:
+        break;
+    }
     return state;
 };
 
