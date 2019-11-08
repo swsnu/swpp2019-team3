86b9ed802537591e77e5259bea4f54c4cf461552
kosumin <be_0302@naver.com>
2019-11-08 23:34:33 +0900

    add-get-me

--- backend/papersfeed/apis.py
+++ backend/papersfeed/apis.py
@@ -23,6 +23,9 @@ def get_session(args):
     """Get Session"""
     return users_utils.select_session(args)
 
+def get_user_me(args):
+    """Get Currentt User"""
+    return users_utils.select_me(args)
 
 def delete_session(args):
     """Delete Session"""
