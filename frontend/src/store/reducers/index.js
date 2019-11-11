ff00ccda72f05367ab55004b24186b3206dc6c6f
Davin <bdv951101@gmail.com>
2019-11-12 03:40:36 +0900

    rename Collection to collection

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 
-import CollectionReducer from "./Collection/CollectionReducer";
+import CollectionReducer from "./collection/CollectionReducer";
 import authReducer from "./auth/auth";
 import paperReducer from "./paper/paper";
 
