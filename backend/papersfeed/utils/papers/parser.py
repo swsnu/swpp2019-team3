b1c3d1045e93660c84020e313db366de71126e16
Davin <bdv951101@gmail.com>
2019-11-13 03:52:21 +0900

    parser with extracting keywords from abstracts

--- backend/papersfeed/utils/papers/parser.py
+++ backend/papersfeed/utils/papers/parser.py
@@ -5,6 +5,9 @@ import json
 import re
 import sys
 from datetime import datetime
+from pprint import pprint
+
+from abstract_analysis import get_key_phrases
 
 csv.field_size_limit(sys.maxsize)
 
@@ -36,7 +39,7 @@ MONTH_DICT = {
 # pylint: disable=too-many-locals, too-many-statements
 def get_papers(filename):
     """
-    Parser for tab seperated text files downloaded from Web Of Science
+    Parser for tab seperated text files downloaded from Web of Science
     Referencing cs_10.txt or cs_500.txt would be helpful for understanding this function
     Also, you can check all field tags in https://images.webofknowledge.com/images/help/WOS/hs_wos_fieldtags.html
     """
@@ -65,6 +68,9 @@ def get_papers(filename):
     paper_publications = []
     keywords = []
     paper_keywords = []
+
+    abstracts = {}
+    keywords_dict = {}
 
     address_pattern = re.compile(r'(\[.*?\].*?)\;')
     address_author_pattern = re.compile(r'\[.*?\]')
@@ -199,27 +205,73 @@ def get_papers(filename):
         }
         paper_publications.append(paper_publication)
 
-        # 7. create a Keyword record
+        # 7-1. save abstracts with key of paper for extracting keywords later
+        abstracts[pk['paper']] = row['AB']
+
+        # 7-2.
         keywords_name = list(map(str.strip, row['DE'].split(';')))
         for keyword_name in keywords_name:
-            pk['keyword'] += 1
-            keyword_fields = {
-                "name": keyword_name.strip()
-            }
-            keyword_fields.update(creation_and_modification_date)
-            keyword = {
-                "model": "papersfeed.keyword",
-                "pk": pk['keyword'],
-                "fields": keyword_fields
-            }
-            keywords.append(keyword)
+            if not keyword_name:
+                continue
+            keyword_name = keyword_name.lower()
+            if keyword_name in keywords_dict:
+                keywords_dict[keyword_name].append((pk['paper'], 'au'))
+            else:
+                keywords_dict[keyword_name] = [(pk['paper'], 'au')]
 
+
+    doc_list = []
+    request_len = 0
+    request_cnt = 0
+    for paper_key in abstracts:
+        request_len += min(len(abstracts[paper_key]), 5120)
+
+        if request_len >= 500000:
+            documents = {"documents": doc_list}
+            key_phrases = get_key_phrases(documents)
+            request_cnt += 1
+
+            # To check struct of response struct, refer to
+            # https://koreacentral.dev.cognitive.microsoft.com/docs/services/TextAnalytics-v2-1/operations/56f30ceeeda5650db055a3c6
+            process_key_phrases(key_phrases, keywords_dict, request_cnt)
+
+            doc_list = []
+            request_len = min(len(abstracts[paper_key]), 5120)
+
+        doc_list.append({"id": str(paper_key), "language": "en", "text": abstracts[paper_key][:5120]})
+
+    if doc_list:
+        documents = {"documents": doc_list}
+        key_phrases = get_key_phrases(documents)
+        request_cnt += 1
+        process_key_phrases(key_phrases, keywords_dict, request_cnt)
+
+    print("--- Sent {} requests".format(request_cnt))
+
+
+    # create Keyword and PaperKeyword records from keywords_dict
+    for keyword_name in keywords_dict:
+        # 7. create a Keyword record
+        pk['keyword'] += 1
+        keyword_fields = {
+            "name": keyword_name.strip()
+        }
+        keyword_fields.update(creation_and_modification_date)
+        keyword = {
+            "model": "papersfeed.keyword",
+            "pk": pk['keyword'],
+            "fields": keyword_fields
+        }
+        keywords.append(keyword)
+
+        for paper_key_and_type in keywords_dict[keyword_name]:
             # 8. create a PaperKeyword record
             pk['paper_keyword'] += 1
+            keyword_type = "author" if paper_key_and_type[1] == "au" else "abstract"
             paper_keyword_fields = {
-                "paper": pk['paper'],
+                "paper": paper_key_and_type[0],
                 "keyword": pk['keyword'],
-                "type": "author",
+                "type": keyword_type,
             }
             paper_keyword_fields.update(creation_and_modification_date)
             paper_keyword = {
@@ -237,6 +289,20 @@ def get_papers(filename):
     json.dump(models, open(json_filename, 'w'), indent=4)
 # pylint: enable=too-many-locals, too-many-statements
 
+def process_key_phrases(key_phrases, keywords_dict, request_cnt):
+    if key_phrases['errors']:
+        print("- Request {} error".format(request_cnt))
+        pprint(key_phrases['errors'])
+
+    if key_phrases['documents']:
+        for result in key_phrases['documents']:
+            for key_phrase in result['keyPhrases']:
+                key_phrase = key_phrase.lower()
+                paper_key = int(result['id'])
+                if key_phrase in keywords_dict and (paper_key, 'au') not in keywords_dict[key_phrase]:
+                    keywords_dict[key_phrase].append((paper_key, 'ab'))
+                elif key_phrase not in keywords_dict:
+                    keywords_dict[key_phrase] = [(paper_key, 'ab')]
 
 if __name__ == '__main__':
     # pylint: disable=invalid-name
