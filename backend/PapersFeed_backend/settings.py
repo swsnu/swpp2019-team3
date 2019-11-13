8749807fcefb1e751ed537dd63bd9ce17468fcd3
한상범 <>
2019-11-13 18:14:38 +0900

    apidoc setting

--- backend/PapersFeed_backend/settings.py
+++ backend/PapersFeed_backend/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Session
 SESSION_COOKIE_AGE = 60 * 30
