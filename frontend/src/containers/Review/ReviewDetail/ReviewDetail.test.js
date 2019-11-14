a37208e79322d3dd47998fb6a164eb3933cb2fa4
Davin <bdv951101@gmail.com>
2019-11-15 07:57:53 +0900

    collectiondetail like/unlike

--- frontend/src/containers/Review/ReviewDetail/ReviewDetail.test.js
+++ frontend/src/containers/Review/ReviewDetail/ReviewDetail.test.js
@@ -7,77 +7,91 @@ import { reviewStatus, getMeStatus } from "../../../constants/constants";
 import { getMockStore } from "../../../test-utils/mocks";
 import { history } from "../../../store/store";
 
-const stubInitialState = {
-    paper: {
-    },
-    auth: {
-        getMeStatus: getMeStatus.NONE,
-        me: { id: 1 },
-    },
-    collection: {
-
-    },
-    review: {
-        make: {
-            status: reviewStatus.NONE,
-            review: {},
-            error: null,
-        },
-        list: {
-            status: reviewStatus.NONE,
-            list: [],
-            error: null,
-        },
-        edit: {
-            status: reviewStatus.NONE,
-            review: {},
-            error: null,
-        },
-        delete: {
-            status: reviewStatus.NONE,
-            review: {},
-            error: null,
-        },
-        selected: {
-            status: reviewStatus.NONE,
-            review: { id: 1 },
-            error: null,
-            replies: [],
-        },
-    },
-};
+const mockPromise = new Promise((resolve) => { resolve(); });
 
 describe("<ReviewDetail />", () => {
+    let stubInitialState;
     let reviewDetail;
-    let spyOnGetMe;
     let spyOnGetReview;
+    let spyLikeReview;
+    let spyUnlikeReview;
 
     beforeEach(() => {
+        stubInitialState = {
+            paper: {
+            },
+            auth: {
+                getMeStatus: getMeStatus.NONE,
+                me: { id: 1 },
+            },
+            collection: {
+
+            },
+            review: {
+                make: {
+                    status: reviewStatus.NONE,
+                    review: {},
+                    error: null,
+                },
+                list: {
+                    status: reviewStatus.NONE,
+                    list: [],
+                    error: null,
+                },
+                edit: {
+                    status: reviewStatus.NONE,
+                    review: {},
+                    error: null,
+                },
+                delete: {
+                    status: reviewStatus.NONE,
+                    review: {},
+                    error: null,
+                },
+                selected: {
+                    status: reviewStatus.NONE,
+                    review: { id: 1 },
+                    error: null,
+                    replies: [],
+                },
+                like: {
+                    status: reviewStatus.NONE,
+                    count: 0,
+                    error: null,
+                },
+                unlike: {
+                    status: reviewStatus.NONE,
+                    count: 0,
+                    error: null,
+                },
+            },
+        };
+
         reviewDetail = (
             <Provider store={getMockStore(stubInitialState)}>
                 <ReviewDetail match={{ params: { review_id: 1 } }} history={history} />
             </Provider>
         );
-        spyOnGetMe = jest.spyOn(authActions, "getMe")
-            .mockImplementation(() => () => new Promise(
-                (resolve) => { resolve({ status: 200 }); },
-            ));
         spyOnGetReview = jest.spyOn(reviewActions, "getReview")
             .mockImplementation(() => () => new Promise(
                 (resolve) => { resolve({ status: 200 }); },
             ));
+        spyLikeReview = jest.spyOn(reviewActions, "likeReview")
+            .mockImplementation(() => () => mockPromise);
+        spyUnlikeReview = jest.spyOn(reviewActions, "unlikeReview")
+            .mockImplementation(() => () => mockPromise);
     });
 
     afterEach(() => {
         jest.clearAllMocks();
     });
 
+
     it("should render without errors", () => {
         const component = mount(reviewDetail);
         const wrapper = component.find("ReviewDetail");
         expect(wrapper.length).toBe(1);
         expect(spyOnGetReview).toHaveBeenCalledTimes(1);
-        expect(spyOnGetMe).toHaveBeenCalledTimes(1);
     });
 
     it("should handle change on new reply", () => {
@@ -93,28 +107,28 @@ describe("<ReviewDetail />", () => {
         expect(instance.state.newReply).toBe("ABC");
     });
 
-    it("should handle Like/Unlike Button", () => {
+    it("should call likeReview when Like Button is clicked", () => {
         const component = mount(reviewDetail);
-        component.setProps({
-            selectedReview: { id: 1 },
-            auth: { id: 1 },
-        });
-
-        const instance = component.find("ReviewDetail").instance();
-
-        const wrapper = component.find(".review-extra .like-button").hostNodes();
+        const wrapper = component.find(".like-button").hostNodes();
         expect(wrapper.length).toBe(1);
-        wrapper.simulate("click");
-
-        component.update();
-
-        expect(instance.state.thisReview.liked).toBe(true);
-        expect(instance.state.likeCount).toEqual(1);
 
         wrapper.simulate("click");
+
+        expect(spyLikeReview).toHaveBeenCalledTimes(1);
+    });
+
+    it("should call unlikeReview when IsLiked and Like Button is clicked", () => {
+        const component = mount(reviewDetail);
+        const instance = component.find(ReviewDetail.WrappedComponent).instance();
+        instance.setState({ isLiked: true });
         component.update();
-        expect(instance.state.likeCount).toEqual(0);
-        expect(instance.state.thisReview.liked).toBe(false);
+
+        const wrapper = component.find(".like-button").hostNodes();
+        expect(wrapper.length).toBe(1);
+
+        wrapper.simulate("click");
+
+        expect(spyUnlikeReview).toHaveBeenCalledTimes(1);
     });
 
     it("should handle edit button", () => {
