f32eead0486c26ca009f2765a294741883bd38a3
Davin Byeon <bdv111@snu.ac.kr>
2019-11-17 00:25:38 +0900

    Revert "csrf_setting (#84)"
    This reverts commit 179b57b0defb6560ac5edeffe7331601212ccd9c.

--- backend/papersfeed/views.py
+++ backend/papersfeed/views.py
@@ -6,9 +6,9 @@ import json
 import traceback
 
 # Django Modules
-from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt
 
 # Internal Modules
 from papersfeed.utils.base_utils import ApiError
@@ -22,16 +22,7 @@ def api_not_found():
     raise ApiError(404)
 
 
-@ensure_csrf_cookie
-def token(request):
-    """token"""
-    if request.method == 'GET':
-        return HttpResponse(status=204)
-
-    return HttpResponseNotAllowed(['GET'])
-
-
-# @ensure_csrf_cookie
+@csrf_exempt
 def api_entry(request, api, second_api=None, third_api=None, fourth_api=None):
     """api_entry"""
     # API 요청에 Return 할 Response Initialize
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import axios from "axios";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -17,9 +16,6 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("root"),
 );
-
-axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-axios.defaults.xsrfCookieName = "csrftoken";
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
