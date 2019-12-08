7f3e410ce4b26d83ab4f7103538fe5b20f97a64a
kosumin <be_0302@naver.com>
2019-12-09 02:18:48 +0900

    dfa

--- frontend/src/containers/Subscription/SubscriptionFeed.js
+++ frontend/src/containers/Subscription/SubscriptionFeed.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
-
+//
 import { PaperCard, CollectionCard, ReviewCard } from "../../components";
 import { authActions } from "../../store/actions";
 
