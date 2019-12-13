b1cb4fc371142851752b479fbc1cbc3b58be073f
kosumin <be_0302@naver.com>
2019-12-14 00:18:09 +0900

    sidebar

--- frontend/src/components/SideBar/SideBar.test.js
+++ frontend/src/components/SideBar/SideBar.test.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import SideBar from "./SideBar";
 
 const mockHistory = { push: jest.fn(), goBack: jest.fn(), location: { pathname: "/main" } };
 describe("<SideBar />", () => {
     it("should render without erros", () => {
-        const component = shallow(<SideBar history={mockHistory} />);
+        const component = shallow(<SideBar.WrappedComponent history={mockHistory} />);
         const wrapper = component.find(".sidebar");
         expect(wrapper.length).toBe(1);
     });
