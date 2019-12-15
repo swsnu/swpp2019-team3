2e7a23b4deb0253a61a95cad69ad4fee0d725d31
kosumin <be_0302@naver.com>
2019-12-16 01:53:53 +0900

    dfa

--- frontend/src/containers/Collection/CollectionDetail/CollectionDetail.test.js
+++ frontend/src/containers/Collection/CollectionDetail/CollectionDetail.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { mount } from "enzyme";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
-
+//
 import { createBrowserHistory } from "history";
 import { collectionActions, replyActions } from "../../../store/actions";
 import { collectionStatus, signinStatus } from "../../../constants/constants";
