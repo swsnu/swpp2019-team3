af13d645d9a8cf47fea528b3201405e22a1ed2f9
Jihyo Kim <njsh4261@snu.ac.kr>
2019-12-16 14:35:42 +0900

    Card component design hotfix (#216)
    * frontend design
    
    * like button
    
    * collection manage modified
    
    * LikeButton applied on Paper/Collection/Review Card components
    
    * slightly modified backend
    
    * modified backend test
    
    * working on card component
    
    * design works
    
    * done with card header text
    
    * modified some button color
    
    * sonar
    
    * modified cards little bit
    
    * buttons

--- frontend/src/components/Button/SubItemButton/SubItemButton.js
+++ frontend/src/components/Button/SubItemButton/SubItemButton.js
@@ -8,7 +8,7 @@ import "./SubItemButton.css";
 const SubItemButton = (props) => (
     <Button
       className="SubItemButton"
-      variant="outline-secondary"
+      variant="light"
       width={props.width}
       height={props.height}
       onClick={props.click}
